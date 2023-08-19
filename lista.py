7791f2932457a32736f33993f2824b087f108334
matteuszera <140860997+matteuszera@users.noreply.github.com>
2023-08-18 21:44:27 -0300

    lista

---
+++ lista.py
@@ -0,0 +1,10 @@
+lista1 = ("mateus", "augusto","Mendonça", "lange")
+print (type(lista1))
+print (len(lista1))
+print (lista1[4])
+lista2 = lista1 *2
+print(lista2)
+
+#adcinonar na lista
+lista1 .append("coisa")
+print (lista1)
---
+++ tupla.py
@@ -0,0 +1,9 @@
+carros = ("gol","fiesta", "uno", "corsa",)
+print (type(carros))
+
+print(carros[0])
+
+print(carros. index("fiesta")) 
+
+for tuplas in carros:
+    print(tuplas) 
